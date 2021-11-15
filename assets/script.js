f9363dd565d595f61cf6bd0b53e3b72747e0cae8
unknown <lopez1432.al@gmail.com>
2021-11-15 11:22:03 -0800

    add JS variables, intro, questions and answers sections

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+// variables for page elements
+// time and score
+let timeEl = document.querySelector("p.time");
+let secondsLeft = 75;
+let scoreEl = document.querySelector("#score");
+
+// sections
+// section intro
+const introEl = document.querySelector("#intro");
+
+// section questions
+//question section
+const questionsEl = document.querySelector("#questions");
+//where question goes
+let questionEl = document.querySelector("#question");
+// how many questions they have answered
+let questionCount = 0;
+// div yaynay
+const yaynayEl = document.querySelector("#yaynay");
+
+// section final
+const finalEl = document.querySelector("#final");
+// user initials
+let initialsInput = document.querySelector("#initials");
+
+// section highscores
+const highscoresEl = document.querySelector("#highscores");
+// ordered list
+let scoreListEl = document.querySelector("#score-list");
+// array of scores
+let scoreList = [];
